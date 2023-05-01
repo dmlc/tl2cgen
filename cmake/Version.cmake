886050fde59b2a380d63d4c6788b37c189ee6564
Hyunsu Cho <chohyu01@cs.washington.edu>
2023-05-01 12:16:59 -0700

    Add script to update version

--- cmake/Version.cmake
+++ cmake/Version.cmake
@@ -1,9 +1,6 @@
 function(write_version)
   message(STATUS "TL2cgen VERSION: ${PROJECT_VERSION}")
   configure_file(
-      ${PROJECT_SOURCE_DIR}/cmake/Python_version.in
-      ${PROJECT_SOURCE_DIR}/python/tl2cgen/VERSION @ONLY)
-  configure_file(
       ${PROJECT_SOURCE_DIR}/cmake/version.h.in
       include/tl2cgen/version.h)
 endfunction(write_version)
