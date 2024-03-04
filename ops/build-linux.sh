bb0b7b4d05d577822b6e29ac5b1528f2606a0731
Hyunsu Cho <chohyu01@cs.washington.edu>
2024-03-04 11:12:41 -0800

    [CI] Fix wheel builders

--- ops/build-linux.sh
+++ ops/build-linux.sh
@@ -4,12 +4,13 @@ set -euo pipefail
 
 TAG=manylinux2014_x86_64
 
-echo "##[section]Building TL2cgen..."
-ops/docker/ci_build.sh cpu ops/scripts/build_via_cmake.sh
+export CIBW_BUILD=cp38-manylinux_x86_64
+export CIBW_ARCHS=x86_64
+export CIBW_BUILD_VERBOSITY=3
+export CIBW_MANYLINUX_X86_64_IMAGE=manylinux2014
 
-echo "##[section]Packaging Python wheel for TL2cgen..."
-ops/docker/ci_build.sh cpu bash -c "cd python/ && pip wheel --no-deps -v . --wheel-dir dist/"
-ops/docker/ci_build.sh auditwheel_x86_64 auditwheel repair --only-plat --plat ${TAG} python/dist/*.whl
-rm -v python/dist/*.whl
+echo "##[section]Building Python wheel (amd64) for Treelite..."
+python -m cibuildwheel python --output-dir wheelhouse
+python ops/scripts/rename_whl.py wheelhouse ${COMMIT_ID} ${TAG}
+mkdir -vp python/dist/
 mv -v wheelhouse/*.whl python/dist/
-ops/docker/ci_build.sh cpu python ops/scripts/rename_whl.py python/dist ${COMMIT_ID} ${TAG}
